7bdc45483034dabe3aa341079ea13a568c49c9ce
xuzhipeng <1071556447@qq.com>
2024-06-10 14:32:33 +0800

    UIPlayerHUD bindWidget

--- Source/XuKit/UI/IUIBase/UIPlayerHUD.h
+++ Source/XuKit/UI/IUIBase/UIPlayerHUD.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
 #include "XuKit/UI/UIBaseInterface.h"
 #include "UIPlayerHUD.generated.h"
 
@@ -18,11 +20,42 @@ private:
 	GENERATED_BODY()
 
 public:
+
+//HP、Shield、Rush
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UProgressBar* shield_progress_bar;
+	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UProgressBar* Health_progress_bar;
+	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UProgressBar* Rush_progress_bar;
 	
 	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* ClipAmmoNum_Text;
+	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* AllAmmoNum_Text;
 	
 	
+//XP、Coin
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UProgressBar* XP_progress_bar;
 	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* XP_Text;
+
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* Coin_Text;
+
+	
+//Task
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* TaskTitle_Text;
+
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* TaskContent_Text;
 
 protected:
 
