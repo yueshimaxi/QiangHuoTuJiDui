4f672fa1416bd8454a898a0e28aae493f3283d84
yueshimaxi <1071556447@qq.com>
2024-08-12 23:27:08 +0800

    添加PackageRedirects，添加damageUI

---
+++ Source/XuKit/UI/AuraUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XUKIT_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
